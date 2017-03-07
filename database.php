be7f009ac30ddc0510eab507924ea54d557a8c12
sarahvassello <sarah.vassello@gmail.com>
2017-03-07 18:11:25 -0500

    Added additional error reporting.

--- database.php
+++ database.php
@@ -1,3 +1,13 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=app;charset=utf8mb4', 'root', 'root');
+
+error_reporting(E_ALL);
+
+if (!ini_get('display_errors')) {
+  ini_set('display_errors', 1);
+}
+
+$db = new PDO('mysql:host=localhost;dbname=contactManager;charset=utf8mb4', 'root', 'root');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 ?>
