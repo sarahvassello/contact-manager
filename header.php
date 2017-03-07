54808ebf236b7417ac75be065bb846fd897172a0
sarahvassello <sarah.vassello@gmail.com>
2017-03-07 18:14:38 -0500

    Updated bootstrap code to prevent repetition.

--- header.php
+++ header.php
@@ -8,12 +8,9 @@
 
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styles.css">
 
   </head>
   <body>
     <div class="container">
       <div class="row col-md-6 col-md-offset-3">
-
-  </body>
-</html>
