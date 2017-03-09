4ae45d9dc45cf81ee9634000cbc0f9871241e39e
sarahvassello <sarah.vassello@gmail.com>
2017-03-09 18:29:07 -0500

    Added navbar to header.php.

--- header.php
+++ header.php
@@ -9,6 +9,30 @@
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Faster+One|Kumar+One+Outline|Sail|Sancreek|Source+Sans+Pro:300" rel="stylesheet">
+
+
+
+<!-- navbar -->
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<a class="navbar-brand" href="index.php">Contact Manager</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="new.php">New Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">All Contacts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/stylesheet.php">Stylesheet</a>
+      </li>
+    </ul>
+ </div>
+</nav>
 
   </head>
   <body>
