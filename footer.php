b77f3bb9183aae1aa3125b9bfa00e554909e6c34
sarahvassello <sarah.vassello@gmail.com>
2017-03-10 16:44:32 -0500

    Finished credits, links to github.

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<div class="row col-md-6 col-md-offset-3">
+<p class="center">Created by <a href="http://sarahmvassello.com/">Sarah Vassello</a>. Photo credit to the <a href="http://ackland.org/">Ackland Art Museum</a>. Find the project on <a href="https://github.com/sarahvassello/contact-manager">github</a> and view the <a href="/stylesheet.php">style guide</a> here.
+</div>
+
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
