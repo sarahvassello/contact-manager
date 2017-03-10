c8f831d66dd63470e3c450b19b4e0d7905d12990
sarahvassello <sarah.vassello@gmail.com>
2017-03-10 16:36:02 -0500

    Completed style sheet.

---
+++ stylesheet.php
@@ -0,0 +1,103 @@
+<?php
+  include 'header.php'
+?>
+  <body>
+    <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+    <h1>Colors</h1>
+  </div>
+  <div class="space">
+    <div class="row col-md-6">
+      <div class="box dark"></div>
+      <div class="box light"></div>
+      <div class="box red"></div>
+      <div class="box yellow"></div>
+      <div class="box cream"></div>
+    </div>
+    <div class="row col-md-6">
+    <div class="box white1"></div>
+    <div class="box white2"></div>
+    <div class="box white3"></div>
+    <div class="box white4"></div>
+    <div class="box white5"></div>
+  </div>
+</div>
+    </div>
+  <div class="row">
+    <div class="col-md-3">
+    <h1>Type</h1>
+  </div>
+    <div class="row col-md-6 ">
+    <h1> h1. Source Sans Pro, 3.5em </h1>
+    <h2> h2. Source Sans Pro, 2.5 em </h2>
+    <h3> h3. Source Sans Pro, bold </h3>
+    <h4> h4. Source Sans Pro </h4>
+    <h5> h5. Source Sans Pro, yellow </h5>
+    <h6> h6. Source Sans Pro</h6>
+    <p><p class="lead">This is paragraph text. It's the lead paragraph text, to be specific. </p><b>This is bold. </b>It is used to create paragraphs. Everyone likes paragraphs
+      because they are the best. They are so full of information. Please use paragraphs to communicate
+      in your everyday life and on web communications. This is Source Sans Pro. <p id="light">This is light.</p></p>
+      <ul><b>This is an unordered list.</b>
+        <li>Here's a point.</li>
+        <li>Here's another.</li>
+        <li>Good things always come in threes.</li>
+      </ul>
+        <ol><b>This is an ordered list.</b>
+          <li>Here's a point.</li>
+          <li>Here's another.</li>
+          <li>I think we get it by now.</li>
+        </ol>
+      </div>
+    </div>
+  <div class="row">
+    <div class="col-md-3">
+    <h1>Buttons</h1>
+  </div>
+  <div class="col-md-8">
+    <button class="smallb">small button</button>
+    <button class="mediumb">medium button</button>
+    <button class="largeb">large button</button>
+    <button class="outline1">outline 1</button>
+    <button class="outline2">outline 2</button>
+  </div>
+</div>
+  <div class="row">
+    <div class="col-md-3">
+      <h1>Forms</h1>
+  </div>
+  <div class="col-md-6">
+    <div class="col-lg-6">
+      <div class="form-group">
+        <label for="contact_last">Smaller Form</label>
+        <input class="form-control" type="text" name="last" id="contact_last" value="" placeholder="col-6" />
+      </div>
+    </div>
+    <div class="col-lg-12">
+      <div class="form-group">
+        <label for="contact_title">Large Form</label>
+        <input class="form-control" type="text" name="title" id="contact_title" value="" placeholder="col-12" />
+      </div>
+    </div>
+    <div class="col-md-6">
+      <select>
+  <option value="one">One</option>
+  <option value="two">Two</option>
+  <option value="three">Three</option>
+  <option value="four">Four</option>
+</select>
+<p> This is a select form.</p>
+    </div>
+    <div class="row">
+    <div class="col-md-6">
+      <form action="#">
+  Text input <input type="text" name="lname">
+  <input type="submit" value="Submit">
+</form>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+  </body>
